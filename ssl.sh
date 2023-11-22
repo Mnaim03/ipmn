279895ac4238c9adacce6d49a6d283efe0173b57
Mnaim03 <99801017+Mnaim03@users.noreply.github.com>
2023-11-22 23:05:23 +0100

    updt

--- ssl.sh
+++ ssl.sh
@@ -5,12 +5,12 @@ echo "\033[35m########################################\033[0m"
 echo "\033[35m################ START  ################\033[0m"
 echo "\033[35m########################################\033[0m"
 #START
-  cd /Users/mohamadnaim/Desktop/py-req  #zona attuale dove si trova la mia cartella
+  cd /Users/mohamadnaim/Documents/pyreq  #zona attuale dove si trova la mia cartella
   chmod +x main.py  #do i permessi al main.py
   chmod +x ch_call.sh #do i permessi al file bash che chiama ch_get e ch_write
   python3 main.py 
  
-  #cd /Users/mohamadnaim/Desktop/py-req/ch
+  #cd /Users/mohamadnaim/Documents/pyreq/ch
   #open -a VLC mono.m3u8
 
 #SAVE
