5140070cd1bb67de594385a8cb79ae32de92a2e8
Mnaim03 <99801017+Mnaim03@users.noreply.github.com>
2023-11-23 15:07:28 +0100

    Update

--- ssl.sh
+++ ssl.sh
@@ -5,10 +5,10 @@ echo "\033[35m########################################\033[0m"
 echo "\033[35m################ START  ################\033[0m"
 echo "\033[35m########################################\033[0m"
 #START
-  # cd /Users/mohamadnaim/Documents/pyreq  #zona attuale dove si trova la mia cartella
-  # chmod +x main.py  #do i permessi al main.py
-  # chmod +x ch_call.sh #do i permessi al file bash che chiama ch_get e ch_write
-  # python3 main.py 
+  cd /Users/mohamadnaim/Documents/pyreq  #zona attuale dove si trova la mia cartella
+  chmod +x main.py  #do i permessi al main.py
+  chmod +x ch_call.sh #do i permessi al file bash che chiama ch_get e ch_write
+  python3 main.py 
  
 
 echo ""
@@ -16,11 +16,11 @@ echo "\033[35m########################################\033[0m"
 echo "\033[35m################# SAVE #################\033[0m"
 echo "\033[35m########################################\033[0m"
  #SAVE
-  # cd /Users/mohamadnaim/Documents/pyreq
-  # chmod +x mono.py
-  # python3 mono.py
-  # #open -a VLC mono.m3u8
-  # echo "\033[32m File mono aggiornato \033[0m"
+  cd /Users/mohamadnaim/Documents/pyreq
+  chmod +x mono.py
+  python3 mono.py
+  #open -a VLC mono.m3u8
+  echo "\033[32m File mono aggiornato \033[0m"
 echo "" 
 echo "\033[35m########################################\033[0m"
 echo "\033[35m################# GIT ##################\033[0m"
