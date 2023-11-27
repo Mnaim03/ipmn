599b1cadecb738a6edec1c7b2e750ad14b89af4f
Mnaim03 <99801017+Mnaim03@users.noreply.github.com>
2023-11-27 16:26:53 +0100

    starter and driver update

---
+++ starters/starter(linux).sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#starter linux
+cd #percorso
+chmod +x ssl.sh 
+
+#if 1--> mac refresh
+#if 2--> server refresh
+#if 3--> mac film add+
+./ssl.sh 2
--- starters/starter(mac).sh
+++ starters/starter(mac).sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+#starter mac
 cd /Users/mohamadnaim/Documents/pyreq
 chmod +x ssl.sh 
+
+#if 1--> mac refresh
+#if 2--> server refresh
+#if 3--> mac film add+
 ./ssl.sh 1
