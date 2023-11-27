eba2108859b3d32070b304c1813c8e6eb73af427
Mnaim03 <99801017+Mnaim03@users.noreply.github.com>
2023-11-27 11:32:10 +0100

    Update

--- ch_call.sh
+++ ch_call.sh
@@ -9,19 +9,29 @@ file="$1"
 tvg_id="$2"
 link_m3u8="$3"
 stream="$4" #zone del file del canale
- 
+i=0
+
 echo " "
 echo "AVVIO $tvg_id"
 
 if [ -z "$link_m3u8" ]; then
-  while [ -z "$link_m3u8" ]; do
+  while [ -z "$link_m3u8" ] && ["$i" -ne 10]; do
+    #Incremento contatore
+    ((i++))
+
     # Do i permessi al driver chrome
-    echo -e "\033[33m ! cerco m3u8 $tvg_id \033[0m"
+    echo -e "\033[33m ! cerco m3u8 $tvg_id -> tentativo $i \033[0m"
     chmod +x ch_get.py
     link_m3u8=$(python3 ch_get.py "$stream")
+
   done
 else
     echo -e "\033[32m ! m3u8 pre-inserito \033[0m"
+fi
+
+#verifico errore 
+if [ -z "$link_m3u8" ]; then
+    #errore.py   #errore da segnale in error.txt
 fi
 
 echo "! scrivo m3u8 $tvg_id su $file"
