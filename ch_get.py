69af669408056f8a3b9785833524d85b87fe2c97
Mnaim03 <99801017+Mnaim03@users.noreply.github.com>
2023-11-30 23:19:54 +0100

    Update

--- ch_get.py
+++ ch_get.py
@@ -12,7 +12,7 @@ code= int(sys.argv[2]) #codice per identificare sistem
 
 # Imposta la variabile d'ambiente per il percorso del driver di Chrome
 
-if i==1: #MacOS
+if code==1: #MacOS
  chrome_driver_path = "chrome/chromedriver(mac)"
 else: #Linux (=2≠1)
  chrome_driver_path = "chrome/chromedriver(linux)"
