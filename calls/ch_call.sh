542a31d55582390c4dee7d47e49f82207fdfc47e
Mnaim03 <99801017+Mnaim03@users.noreply.github.com>
2023-11-30 10:56:14 +0100

    Update

---
+++ calls/ch_call.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+chmod +x ch_call.sh #gli faccio autodare i permessi
+#per dare i permessi via shell: chmod +x eseg.py
+
+#i seguenti elementi devono essere passati goni volta da main.py
+#metto dei valori prova
+
+file="$1" 
+tvg_id="$2"
+link_m3u8="$3"
+stream="$4" #zone del file del canale
+i=0
+
+echo " "
+echo "AVVIO $tvg_id"
+
+if [ -z "$link_m3u8" ]; then
+  while [ -z "$link_m3u8" ] && [ "$i" -ne 10 ]; do
+    #Incremento contatore
+    ((i++))
+
+    # Do i permessi al driver chrome
+    echo -e "\033[33m ! cerco m3u8 $tvg_id -> tentativo $i \033[0m"
+    chmod +x ch_get.py
+    link_m3u8=$(python3 ch_get.py "$stream")
+
+  done
+else
+    echo -e "\033[32m ! m3u8 pre-inserito \033[0m"
+fi
+
+#verifico errore 
+if [ -z "$link_m3u8" ]; then
+    echo "! SEGNALO ERRORE"
+    chmod +x ch/out/error.py
+    python3 ch/out/error.py  0 "$tvg-id" "$stream" #errore da segnale in error.txt
+fi
+
+echo "! scrivo m3u8 $tvg_id su $file"
+chmod +x ch_write.py 
+python3 ch_write.py "$file" "$tvg_id"  "$link_m3u8"
+echo "FINE $tvg_id"
---
+++ calls/ch_get.py
@@ -0,0 +1,45 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import os
+import sys #estrazione bash
+import time
+
+#estrazione link
+url = sys.argv[1]
+
+# Imposta la variabile d'ambiente per il percorso del driver di Chrome
+chrome_driver_path = "chrome/chromedriver(mac)"
+os.environ['PATH'] += ":" + chrome_driver_path
+
+# Opzioni del browser Chrome
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_extension('chrome/sniffer.crx')
+
+# Inizializzazione del driver di Chrome
+browser = webdriver.Chrome(options=chrome_options)
+
+browser.get(url)
+
+# Attendi il caricamento completo dell'estensione
+wait_extension = WebDriverWait(browser, 20)
+extension_loaded = wait_extension.until(EC.presence_of_element_located((By.ID, 'myM3u8LayerId')))
+
+wait = WebDriverWait(browser, 20)
+links = wait.until(EC.presence_of_all_elements_located((By.TAG_NAME, 'a')))
+
+for link in links:
+    try:
+        href = link.get_attribute('href')
+        if href and ('javascript:' not in href):
+
+            #la condizione dell'if è il filtro dei vari link
+            if href.startswith('https://cdn1.ipslow.com/tv') or href.startswith('https://tv.ipslow.com/tv') or href.endswith('.m3u8') or href.startswith('https://playback2.akamaized'):
+                print(href)  # Stampa il link se soddisfa i criteri
+    except StaleElementReferenceException:
+        print("Elemento non più valido, continuo con il prossimo elemento...")
+        continue
+
+time.sleep(6) #protezione anti ip-ban
+browser.quit()
