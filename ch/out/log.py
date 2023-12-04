47bffb83861803538c222a57de4daa95cee8c6bb
Mnaim03 <99801017+Mnaim03@users.noreply.github.com>
2023-12-04 22:10:43 +0100

    Update

--- ch/out/log.py
+++ ch/out/log.py
@@ -8,7 +8,7 @@ def data(): # Ottieni data atuale
 
 def txtprint(host): #stampa nel file
   with open('ch/out/log.txt', 'a') as file:
-    file.write(f"\n {time} -----> {host}")
+    file.write(f'\n <br><p class="txtlog">🔋<font class="smalldata">{time}</font>:{host}</p><br>')
 
 #main
 host = sys.argv[1]
