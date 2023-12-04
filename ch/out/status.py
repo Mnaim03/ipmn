bb7d85b2029622272ca116339fb8744e9408fc12
Mnaim03 <99801017+Mnaim03@users.noreply.github.com>
2023-12-04 14:12:07 +0100

    Update

--- ch/out/status.py
+++ ch/out/status.py
@@ -9,13 +9,14 @@ def data(): # Ottieni data atuale
 
 def txtprintw(): #stampa nel file
   with open('ch/out/status.txt', 'a') as file:
-    file.write(f"\n\n {tvgid} --> Estrazione m3u8 ESEGUITA CORRETTAMENTE (✓).")
-    file.write(f"\n Link stream fornito: {stream}")
+    file.write(f"\n\n<div class=""content container text-center""><p class=""h7""><font class=""channel h2""> {tvgid} </font>")
+    file.write(f"\n<a href=""{stream}""><button type="button" class=""btn btn-success btnlink"">ESTAZIONE m3u8 ESEGUITA(✓)</button></a></p></div>")
 
 def txtprintf(): #stampa nel file
   with open('ch/out/status.txt', 'a') as file:
-    file.write(f"\n\n {tvgid} --> Estrazione m3u8 ANDATA MALE (x). ")
-    file.write(f"\n Link stream fornito: {stream}")
+    file.write(f"\n\n<div class=""content container text-center""><p class=""h7""><font class=""channel h2""> {tvgid} </font>")
+    file.write(f"\n<a href=""{stream}""><button type="button" class=""btn btn-danger btnlink"">ESTAZIONE m3u8 NON ESEGUITA(x)</button></a></p></div>")
+
 
 def txtwipe(): #pulisci file
     with open('ch/out/status.txt', 'r+') as file:
@@ -23,7 +24,7 @@ def txtwipe(): #pulisci file
 
 def print_user():
     with open('ch/out/status.txt', 'a') as file:
-      file.write(f"\n {time} -->{user} has done a update: ")
+      file.write(f"\n<div class=""content container text-center""><p><font class=""bigdata"">{time}</font><br><font class=""user"">{user} has done a update</font></p></div>")
 #main
 i = int(sys.argv[1])
 #se i=0 --> scrivi
