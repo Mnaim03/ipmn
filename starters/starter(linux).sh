bc6a851fdd53a379bc9c666beff1d50812a78c92
Mnaim03 <99801017+Mnaim03@users.noreply.github.com>
2024-11-18 11:25:41 +0100

    remove

--- starters/starter(linux).sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-#starter linux
-cd #percorso
-chmod +x ssl.sh 
-
-#if 1--> mac refresh
-#if 2--> server refresh
-./ssl.sh 2 "Linux Server (m3u8 update)"
--- starters/starter(mac).sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-#starter mac
-cd /Users/mohamadnaim/Documents/pyreq
-chmod +x ssl.sh 
-
-#if 1--> mac refresh
-#if 2--> server refresh
-./ssl.sh 1 "MacOS (m3u8 update)"
